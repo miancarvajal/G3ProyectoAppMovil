f05911002e942d87f2594ee1c0f7570e11c783ee
Maritza <maritzadiazlizarazo@gmail.com>
2021-11-24 08:55:55 -0500

    proyectoSprint2
    Anexo carpeta Sprint 2

---
+++ Sprint_2_G13-TuProfe/TuProfe2/app/src/main/java/com/example/tuprofe/EleccionTema.java
@@ -0,0 +1,14 @@
+package com.example.tuprofe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EleccionTema extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eleccion_tema);
+    }
+}
---
+++ Sprint_2_G13-TuProfe/TuProfe2/app/src/main/java/com/example/tuprofe/Profes.java
@@ -0,0 +1,28 @@
+package com.example.tuprofe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Profes extends AppCompatActivity {
+    TextView tv_registrar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profes);
+
+        tv_registrar = findViewById(R.id.tv_registrar);
+
+        tv_registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentReg = new Intent(Profes.this,Registro.class);
+                Profes.this.startActivity(intentReg);
+
+            }
+        });
+    }
+}
